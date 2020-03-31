1944cf64dec7a2a3d1962ee8426dfef69506e1e7
Isai-gomez <isaigomezhernandez@gmail.com>
2020-03-30 23:17:04 -0600

    week day

---
+++ js/weekDay.js
@@ -0,0 +1,20 @@
+const daysIn = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thurday",
+  "Friday",
+  "Saturday",
+  ""
+];
+const daysEs = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  ""
+];
--- python/plusMinus.py
+++ python/plusMinus.py
@@ -1,17 +1,20 @@
-arr = [8,1,2,3,-1,-2,-3,0,0]
+arr = [6,[-4,3,-9,0,4,1]]
 def plusMinus(arr):
-    count = 1
+    count = 0
     positive = 0
     negative = 0
     cero = 0
-    while count <= arr[0]:
-        if arr[count] > 0:
+    while count < arr[0]:
+        if arr[1][count] > 0:
             positive +=1
-        if arr[count] < 0:
+        if arr[1][count] < 0:
             negative +=1
-        if arr[count] == 0:
+        if arr[1][count] == 0:
             cero +=1
         count += 1
+    # print(positive)
+    # print(negative)
+    # print(cero)
     print(format(positive/arr[0],'.6f'))
     print(format(negative/arr[0],'.6f'))
     print(format(cero/arr[0],'.6f'))
