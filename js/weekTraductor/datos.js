b9721f2c3069e5d8b1149a715ea7c516be22a879
Isai-gomez <isaigomezhernandez@gmail.com>
2020-03-31 13:06:51 -0600

    usando solid

---
+++ js/weekTraductor/datos.js
@@ -0,0 +1,20 @@
+export const daysIn = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thurday",
+  "Friday",
+  "Saturday",
+  ""
+];
+export const daysEs = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  ""
+];
--- js/weekTraductor/weekDay.js
+++ js/weekTraductor/weekDay.js
@@ -1,34 +1,21 @@
-//Traductor de días en ingles a español
-const daysIn = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thurday",
-  "Friday",
-  "Saturday",
-  ""
-];
-const daysEs = [
-  "Domingo",
-  "Lunes",
-  "Martes",
-  "Miercoles",
-  "Jueves",
-  "Viernes",
-  "Sabado",
-  ""
-];
-// Seleccionar body del DOM
-const section = document.querySelector("body");
-console.log(section);
-// Function crear section
-const createSection = days => {
-  let section = "<section>";
-  days.map(element => {
-    section += `<option>${element}</option>`;
-  });
-  section += "</section>";
-  return section;
-};
-console.log(createSection(daysIn));
+import { daysIn, daysEs } from "./datos.js";
+export function main() {
+  // Seleccionar body del DOM
+  const body = document.querySelector("body");
+
+  // Function crear section
+  const createSection = days => {
+    let section = "<section>";
+    days.map(element => {
+      section += `<option>${element}</option>`;
+    });
+    section += "</section>";
+    return section;
+  };
+  //Function añadir tag al body
+  const addTag = tag => {
+    return (tag.innerHTML = createSection(daysIn));
+  };
+  //main de funnciones
+  addTag(body);
+}
